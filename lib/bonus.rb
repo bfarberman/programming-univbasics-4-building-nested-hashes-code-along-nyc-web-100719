32036d6376e9be772a279c7bbcae3a5154b354ec
Brad Farberman <bfarberman@gmail.com>
2019-09-20 02:10:51 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -27,7 +27,7 @@ def bonus
 
   [:capulet][:heroine][:status] = "dead"
 
-  puts epic_tragedy
+  
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
